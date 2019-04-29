03ff8ced6d7117850b251c4176efe9fb2f09b502
jimmy <u9653022@gmail.com>
2019-04-30 00:58:53 +0800

    add digital number

--- src/components/DigitalNumber.js
+++ src/components/DigitalNumber.js
@@ -1,17 +1,38 @@
 import React from "react";
 import DigitalSign from "./DigitalSign.js";
 import "./digitalNumber.css";
+const DIGITAL = {
+  0: "1111110",
+  1: "0011000",
+  2: "0110111",
+  3: "0111101",
+  4: "1011001",
+  5: "1101101",
+  6: "1101111",
+  7: "1111000",
+  8: "1111111",
+  9: "1111101"
+};
+const DIGITAL_POSITION = [
+  "left-top",
+  "top",
+  "right-top",
+  "right-bottom",
+  "bottom",
+  "left-bottom",
+  "middle"
+];
+export default ({ value }) => {
+  const numberSign = DIGITAL[value];
 
-export default () => {
   return (
     <div styleName="container">
-      <DigitalSign position="left-top" />
-      <DigitalSign position="top" />
-      <DigitalSign position="right-top" />
-      <DigitalSign position="bottom" />
-      <DigitalSign position="left-bottom" />
-      <DigitalSign position="right-bottom" />
-      <DigitalSign position="middle" />
+      {DIGITAL_POSITION.map((position, index) => {
+        const isActive = ~~numberSign[index] ? "active" : "";
+        return (
+          <DigitalSign key={position} position={`${position} ${isActive}`} />
+        );
+      })}
     </div>
   );
 };

0a552e0d443238d7ee6138a7f96cbc86d57c01f4
jimmy <u9653022@gmail.com>
2019-06-09 19:43:36 +0800

    refactory test

--- test/businessLogic/mineMap.test.js
+++ test/businessLogic/mineMap.test.js
@@ -17,6 +17,29 @@ describe("Create MineMap 9*9 and mine 9 ", () => {
     expect(minePosition).toHaveLength(mine);
   };
 
+  const createArraySequence = (from, to) => {
+    return Array.from(Array(to - from + 1).keys()).map(k => k + from);
+  };
+  const assertMapValue = (mineMap, expectData) => {
+    Object.keys(expectData).forEach(position => {
+      expect(mineMap[position]).toEqual(expectData[position]);
+    });
+  };
+  const assertEmptyBrick = (mineMap, from, to) => {
+    const validatePosition = createArraySequence(from, to);
+    Object.keys(validatePosition).forEach(n => {
+      expect(mineMap[validatePosition[n]]).toEqual(0);
+    });
+  };
+  const assertMine = (mineMap, minePosition, expectList) => {
+    const expectObj = expectList.reduce((result, v) => {
+      return {
+        [v]: -1
+      };
+    }, {});
+    assertMapValue(mineMap, expectObj);
+  };
+
   beforeAll(() => {
     math.setupMock();
   });
@@ -34,60 +57,63 @@ describe("Create MineMap 9*9 and mine 9 ", () => {
     const { mineMap, minePosition } = createMineMap(width, height, mine);
 
     // Then
-    expect(mineMap).toHaveLength(width * height);
-    expect(minePosition).toHaveLength(mine);
+    assertMapCorrect(mineMap, minePosition);
   });
 
   test("mine position in first column , and tips correct", () => {
     // Given
+    const expectTips = {
+      9: 2,
+      10: 3,
+      11: 3,
+      12: 3,
+      13: 3,
+      14: 3,
+      15: 3,
+      16: 3,
+      17: 2
+    };
     const expectMinePosition = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     mockRandom(expectMinePosition);
-    const expectTipsFor9To17 = [2, 3, 3, 3, 3, 3, 3, 3, 2];
 
     // When
     const { mineMap, minePosition } = createMineMap(width, height, mine);
 
     // Then
     assertMapCorrect(mineMap, minePosition);
-    expect(minePosition).toEqual(expectMinePosition);
-    for (let i = 0; i < 9; i++) {
-      expect(mineMap[i]).toEqual(-1);
-    }
-    for (let i = 9; i <= 17; i++) {
-      expect(mineMap[i]).toEqual(expectTipsFor9To17[i - 9]);
-    }
-    for (let i = 18; i < mapSize; i++) {
-      expect(mineMap[i]).toEqual(0);
-    }
+    assertMine(mineMap, minePosition, expectMinePosition);
+    assertMapValue(mineMap, expectTips);
+    assertEmptyBrick(mineMap, 18, 80);
   });
 
   test("mine position in last column , and tips correct", () => {
     // Given
+    const expectTips = {
+      63: 2,
+      64: 3,
+      65: 3,
+      66: 3,
+      67: 3,
+      68: 3,
+      69: 3,
+      70: 3,
+      71: 2
+    };
     const expectMinePosition = [72, 73, 74, 75, 76, 77, 78, 79, 80];
     mockRandom(expectMinePosition);
-    const expectTipsFor63To71 = [2, 3, 3, 3, 3, 3, 3, 3, 2];
 
     // When
     const { mineMap, minePosition } = createMineMap(width, height, mine);
 
     // Then
     assertMapCorrect(mineMap, minePosition);
-    expect(minePosition).toEqual(expectMinePosition);
-    for (let i = 72; i < 81; i++) {
-      expect(mineMap[i]).toEqual(-1);
-    }
-    for (let i = 63; i <= 71; i++) {
-      expect(mineMap[i]).toEqual(expectTipsFor63To71[i - 63]);
-    }
-    for (let i = 0; i < 63; i++) {
-      expect(mineMap[i]).toEqual(0);
-    }
+    assertMine(mineMap, minePosition, expectMinePosition);
+    assertMapValue(mineMap, expectTips);
+    assertEmptyBrick(mineMap, 0, 62);
   });
 
   test("mine position in first row , and tips correct", () => {
     // Given
-    const expectMinePosition = [0, 9, 18, 27, 36, 45, 54, 63, 72];
-    mockRandom(expectMinePosition);
     const expectTips = {
       1: 2,
       10: 3,
@@ -99,43 +125,29 @@ describe("Create MineMap 9*9 and mine 9 ", () => {
       64: 3,
       73: 2
     };
+    const expectMinePosition = [0, 9, 18, 27, 36, 45, 54, 63, 72];
+    mockRandom(expectMinePosition);
 
     // When
     const { mineMap, minePosition } = createMineMap(width, height, mine);
 
     // Then
     assertMapCorrect(mineMap, minePosition);
-    expect(minePosition).toEqual(expectMinePosition);
-    expectMinePosition.forEach(m => {
-      expect(mineMap[m]).toEqual(-1);
-    });
-    Object.keys(expectTips).forEach(tipPosition => {
-      expect(mineMap[tipPosition]).toEqual(expectTips[tipPosition]);
-    });
-    const createArraySequence = (from, to) => {
-      return Array.from(Array(to - from + 1).keys()).map(k => k + from);
-    };
-    const assertEmptyBrick = (from, to) => {
-      const validatePosition = createArraySequence(from, to);
-      Object.keys(validatePosition).forEach(n => {
-        expect(mineMap[validatePosition[n]]).toEqual(0);
-      });
-    };
-    assertEmptyBrick(2, 8);
-    assertEmptyBrick(11, 17);
-    assertEmptyBrick(20, 26);
-    assertEmptyBrick(29, 35);
-    assertEmptyBrick(38, 44);
-    assertEmptyBrick(47, 53);
-    assertEmptyBrick(56, 62);
-    assertEmptyBrick(65, 71);
-    assertEmptyBrick(74, 80);
+    assertMine(mineMap, minePosition, expectMinePosition);
+    assertMapValue(mineMap, expectTips);
+    assertEmptyBrick(mineMap, 2, 8);
+    assertEmptyBrick(mineMap, 11, 17);
+    assertEmptyBrick(mineMap, 20, 26);
+    assertEmptyBrick(mineMap, 29, 35);
+    assertEmptyBrick(mineMap, 38, 44);
+    assertEmptyBrick(mineMap, 47, 53);
+    assertEmptyBrick(mineMap, 56, 62);
+    assertEmptyBrick(mineMap, 65, 71);
+    assertEmptyBrick(mineMap, 74, 80);
   });
 
   test("mine position in last row , and tips correct", () => {
     // Given
-    const expectMinePosition = [8, 17, 26, 35, 44, 53, 62, 71, 80];
-    mockRandom(expectMinePosition);
     const expectTips = {
       7: 2,
       16: 3,
@@ -147,36 +159,24 @@ describe("Create MineMap 9*9 and mine 9 ", () => {
       70: 3,
       79: 2
     };
+    const expectMinePosition = [8, 17, 26, 35, 44, 53, 62, 71, 80];
+    mockRandom(expectMinePosition);
 
     // When
     const { mineMap, minePosition } = createMineMap(width, height, mine);
 
     // Then
     assertMapCorrect(mineMap, minePosition);
-    expect(minePosition).toEqual(expectMinePosition);
-    expectMinePosition.forEach(m => {
-      expect(mineMap[m]).toEqual(-1);
-    });
-    Object.keys(expectTips).forEach(tipPosition => {
-      expect(mineMap[tipPosition]).toEqual(expectTips[tipPosition]);
-    });
-    const createArraySequence = (from, to) => {
-      return Array.from(Array(to - from + 1).keys()).map(k => k + from);
-    };
-    const assertEmptyBrick = (from, to) => {
-      const validatePosition = createArraySequence(from, to);
-      Object.keys(validatePosition).forEach(n => {
-        expect(mineMap[validatePosition[n]]).toEqual(0);
-      });
-    };
-    assertEmptyBrick(0, 6);
-    assertEmptyBrick(9, 15);
-    assertEmptyBrick(18, 24);
-    assertEmptyBrick(27, 33);
-    assertEmptyBrick(36, 42);
-    assertEmptyBrick(45, 51);
-    assertEmptyBrick(54, 60);
-    assertEmptyBrick(63, 69);
-    assertEmptyBrick(72, 78);
+    assertMine(mineMap, minePosition, expectMinePosition);
+    assertMapValue(mineMap, expectTips);
+    assertEmptyBrick(mineMap, 0, 6);
+    assertEmptyBrick(mineMap, 9, 15);
+    assertEmptyBrick(mineMap, 18, 24);
+    assertEmptyBrick(mineMap, 27, 33);
+    assertEmptyBrick(mineMap, 36, 42);
+    assertEmptyBrick(mineMap, 45, 51);
+    assertEmptyBrick(mineMap, 54, 60);
+    assertEmptyBrick(mineMap, 63, 69);
+    assertEmptyBrick(mineMap, 72, 78);
   });
 });
